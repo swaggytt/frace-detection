6828725f7a43b7a5753f895302a935ba55cb6760
Puwadol L <alpe_panda@hotmail.co.th>
2021-11-11 03:35:22 +0700

    change weight
    Co-authored-by: NITIKORN TAOTO <swaggytt@users.noreply.github.com>

--- frace.py
+++ frace.py
@@ -5,13 +5,15 @@ from deepface import DeepFace
 import torch
 from torchvision import models, transforms
 import torch.nn as nn
+import time
 
-WEIGHT_PATH = "./resnet-18_weight_v11.pth"
+WEIGHT_PATH = "./resnet-18_weight_v13_gray.pth"
 
 # @st.cache
 def deepface(image_file):
     detected_face = DeepFace.detectFace(image_file, detector_backend='dlib', enforce_detection=False)
-    return detected_face
+    face_img = np.rot90(np.flip(detected_face), 2)
+    return face_img
 
 @st.cache
 def transforms_picture(face_img):
@@ -21,6 +23,7 @@ def transforms_picture(face_img):
     data_transforms = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
+        transforms.Grayscale(),
         transforms.ToTensor(),
     ])
     image = data_transforms(im)
@@ -32,14 +35,20 @@ def create_model(device):
     # load pretrain
     model = models.resnet18()
 
-    # modified model layer
     num_ftrs = model.fc.in_features
-    features = list(model.fc.children())[:-1]  # Remove last layer
-    features.extend([nn.Linear(num_ftrs, 256), nn.ReLU(), nn.Dropout(p=0.8), nn.Linear(256, 3), nn.Softmax(dim=1)])  # Add our layer with 3 outputs
-    model.fc = nn.Sequential(*features)  # Replace the model classifier
+    features = list(model.fc.children())[:-1] # Remove last layer
+    #print(features)
+    features.extend([nn.Flatten(),nn.Linear(num_ftrs, 256),nn.ReLU(),nn.Dropout(p=0.8),nn.Linear(256,3),nn.Softmax(dim=1) ]) # Add our layer with 3 outputs
+    #features = [nn.Linear(num_ftrs, 256),nn.Dropout(p=0.8),nn.Linear(256,len(class_names)) ]  # Add our layer with 3 outputs
+    model.fc = nn.Sequential(*features) # Replace the model classifier
 
-    # load weight
-    model.load_state_dict(torch.load(WEIGHT_PATH, map_location=device))
+    pre_layer = list(model.children())
+    pre_layer.insert(0,nn.Conv2d(1,3,kernel_size=3,stride=1,padding=1,bias=False))# change first layer dim to 1,64 for grayscale
+    model = nn.Sequential(*pre_layer)
+
+    weight = torch.load(WEIGHT_PATH,map_location=device)
+    model.load_state_dict(weight)
+    # load_weight = False
 
     return model 
 
@@ -51,12 +60,11 @@ def predict_race(image, model, device):
     
 
 def main():
-    st.header('Image race predictator')
+    st.header('Image race prediction')
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    file_upload = st.file_uploader("Choose the file", type=['jpg', 'png', 'jpeg', 'jfif'])
+    file_upload = st.file_uploader("Choose a picture to predict", type=['jpg', 'png', 'jpeg', 'jfif'])
     col1, col2 = st.columns([3, 1])
     if file_upload is not None:
-
         with st.spinner('Processing...'):
             # add image 
             image = Image.open(file_upload)
@@ -67,7 +75,7 @@ def main():
 
             # face detection with deepface
             image = deepface(new_img)
-            col2.image(image, channels="BGR", width=200)
+            col2.image(image, width=200)
 
             # transform picture
             transforms_im = transforms_picture(image)
@@ -78,19 +86,18 @@ def main():
             # predict face
             pred = predict_race(transforms_im, model, device)
             _, pred_idx = torch.max(pred.data, 1)
-        if pred_idx == 0:
-            col2.subheader("Predict : Chinese")
-        elif pred_idx == 1:
-            col2.subheader("Predict : Malaysian")
-        else:
-            col2.subheader("Predict : Thai")
-        col2.subheader(f"Probability:")
-        col2.write(f"Chainese {pred.data[0][0]*100:.2f}%")
-        col2.write(f"Malaysian {pred.data[0][1]*100:.2f}%")
-        col2.write(f"Thai {pred.data[0][2]*100:.2f}%")
-        st.success("Success!")
-        # print(f"Probability: \nChainese {pred.data[0][0]*100:.2f}%, \nMalaysian {pred.data[0][1]*100:.2f}%, \nThai {pred.data[0][2]*100:.2f}%")
-        
-
+            if pred_idx == 0:
+                col2.subheader("Predict : Chinese")
+            elif pred_idx == 1:
+                col2.subheader("Predict : Malaysian")
+            else:
+                col2.subheader("Predict : Thai")
+            col2.subheader(f"Probability:")
+            col2.write(f"Chinese {pred.data[0][0]*100:.2f}%")
+            col2.write(f"Malaysian {pred.data[0][1]*100:.2f}%")
+            col2.write(f"Thai {pred.data[0][2]*100:.2f}%")
+            st.success("Success!")
+    else:
+        st.markdown("<h3 style='text-align: center;'>No Result</h1>", unsafe_allow_html=True)
 
 main()
